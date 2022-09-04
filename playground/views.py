a7bdcf575b7c3f243244ba658957844c70679ff1
SalamWeb <abdulsalam.alothman@gmail.com>
2022-09-04 03:48:51 +0000

    Add third parameter in the rendering request app playground in views dictionary, and added some logic in the e hello template

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def say_hello(request):
-  return HttpResponse('hello world')
+ # return HttpResponse('hello world')
+    return render(request, 'hello.html')
 # Create your views here.
