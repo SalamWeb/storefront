a1694b4a0942b0d56f42f3ef0c4ca44e039bed42
SalamWeb <abdulsalam.alothman@gmail.com>
2022-09-09 14:23:44 +0000

    Add debuging  and adding some logic in views.py to render a variable, and add template that to show the debuging

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def say_hello(request):
  # return HttpResponse('hello world')
-    return render(request, 'hello.html')
+    return render(request, 'hello.html', {'name': 'salam'})
 # Create your views here.
